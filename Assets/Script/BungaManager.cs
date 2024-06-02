0fffccc2a0cff7d5dd8a4fe4d1e78a4dfab3d89d
gamesoninternational <gameson.international@gmail.com>
2024-06-03 00:43:13 +0700

    update

---
+++ Assets/Script/BungaManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BungaManager : MonoBehaviour
+{
+    public int BungaCount;
+
+    public GameObject Jumpscare, SajenPanelDone;
+    
+    public int countdownTime = 10; // Waktu hitung mundur dalam detik
+    public TextMeshProUGUI countdownText; // Referensi ke TextMeshPro Text UI
+
+    private Coroutine countdownCoroutine; // Menyimpan referensi ke Coroutine yang berjalan
+
+    public List<GameObject> BungaButton = new List<GameObject>();
+    public List<GameObject> BungaObj = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (countdownCoroutine != null)
+        {
+            StopCoroutine(countdownCoroutine);
+        }
+
+        countdownCoroutine = StartCoroutine(Countdown());
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(BungaCount == 8){
+            SajenPanelDone.SetActive(true);
+            StopCountdown();
+        }
+
+        
+    }
+
+    public void StartGame(){
+        
+    }
+
+    public void StopCountdown()
+    {
+        if (countdownCoroutine != null)
+        {
+            StopCoroutine(countdownCoroutine);
+            countdownText.text = "Stopped"; // Atau teks lain yang Anda inginkan saat dihentikan
+        }
+    }
+
+    private System.Collections.IEnumerator Countdown()
+    {
+        int currentTime = countdownTime;
+
+        while (currentTime > 0)
+        {
+            countdownText.text = "Waktu : " + currentTime.ToString();
+            yield return new WaitForSeconds(1f); // Tunggu 1 detik
+            currentTime--;
+        }
+
+        countdownText.text = "0"; // Tampilkan nol setelah hitungan selesai
+        countdownCoroutine = null; // Reset Coroutine
+        // Anda bisa menambahkan tindakan lain di sini setelah hitungan mundur selesai
+
+        Jumpscare.SetActive(true);
+        StartCoroutine(HideJumpscare());
+        ResetGame();
+        BungaCount = 0;
+    }
+
+    public IEnumerator HideJumpscare(){
+        yield return new WaitForSeconds(3f);
+        Jumpscare.SetActive(false);
+
+        if (countdownCoroutine != null)
+        {
+            StopCoroutine(countdownCoroutine);
+        }
+
+        countdownCoroutine = StartCoroutine(Countdown());
+    }
+
+    public void ResetGame()
+    {
+        foreach (GameObject button in BungaButton)
+        {
+            button.SetActive(true);
+        }
+
+        foreach (GameObject bunga in BungaObj)
+        {
+            bunga.SetActive(false);
+        }
+        
+        Debug.Log("Semua objek telah di-disable.");
+    }
+}
---
+++ Assets/Script/Mahkota.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mahkota : MonoBehaviour
+{
+    public int PermataBiruInt, PermataHijauInt, PermataMerahInt, PermataCount;
+    public int PasangPermata;
+
+    public GameObject MahkotaDone, ClaimButton, CariButton;
+
+    public GameObject PermataBiru, PermataHijau, PermataMerah, Permata;
+
+    public GameObject PermataBiruItem, PermataHijauItem, PermataMerahItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PermataBiruInt = PlayerPrefs.GetInt("PermataBiruInt");
+        PermataHijauInt = PlayerPrefs.GetInt("PermataHijauInt");
+        PermataMerahInt = PlayerPrefs.GetInt("PermataMerahInt");
+        PermataCount = PlayerPrefs.GetInt("PermataCount");
+
+        if(PasangPermata == 3){
+            ClaimButton.SetActive(true);
+            Permata.SetActive(false);
+            CariButton.SetActive(false);
+        }
+
+        if (PermataBiruInt == 1){
+            PermataBiru.SetActive(true);
+            PermataBiruItem.SetActive(false);
+        }
+
+        if (PermataHijauInt == 2){
+            PermataHijau.SetActive(true);
+            PermataHijauItem.SetActive(false);
+        }
+
+        if (PermataMerahInt == 3){
+            PermataMerah.SetActive(true);
+            PermataMerahItem.SetActive(false);
+        }
+
+        if(PermataCount == 3){
+            
+            
+        }
+
+    }
+
+    public void CollectPermataBiru(){
+        PermataBiruInt = 1;
+        PlayerPrefs.SetInt("PermataBiruInt", PermataBiruInt);
+        PermataCount++;
+        PlayerPrefs.SetInt("PermataCount", PermataCount);
+    }
+
+    public void CollectPermataHijau(){
+        PermataHijauInt = 2;
+        PlayerPrefs.SetInt("PermataHijauInt", PermataHijauInt);
+        PermataCount++;
+        PlayerPrefs.SetInt("PermataCount", PermataCount);
+    }
+
+    public void CollectPermataMerah(){
+        PermataMerahInt = 3;
+        PlayerPrefs.SetInt("PermataMerahInt", PermataMerahInt);
+        PermataCount++;
+        PlayerPrefs.SetInt("PermataCount", PermataCount);
+    }
+
+    public void PasangPermataCall(){
+        PasangPermata++;
+    }
+}
